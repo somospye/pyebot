3944f612d97141680280974af87081d5026ea242
nehu3n <developer.nh3@gmail.com>
2025-08-01 01:56:45 -0300

    feat: Add ping command

---
+++ src/commands/ping.ts
@@ -0,0 +1,16 @@
+import type { CommandContext } from "seyfert";
+import { Command, Declare } from "seyfert";
+
+@Declare({
+	name: "ping",
+	description: "Mostrar la latencia con Discord",
+})
+export default class PingCommand extends Command {
+	async run(ctx: CommandContext) {
+		const ping = ctx.client.gateway.latency;
+
+		await ctx.write({
+			content: `La latencia es \`${ping}\``,
+		});
+	}
+}
