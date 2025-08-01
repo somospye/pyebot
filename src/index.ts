3119e85bd51bbcbf16c341310fb1077dd1773de9
nehu3n <developer.nh3@gmail.com>
2025-08-01 01:54:12 -0300

    fix: Add seyfert module declaration

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
+import type { ParseClient } from "seyfert";
 import { Client } from "seyfert";
 
 const client = new Client();
 client.start();
+
+declare module "seyfert" {
+	interface UsingClient extends ParseClient<Client<true>> {}
+}
