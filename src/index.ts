c5288f4e97e80ba46b9042ef3bf88d49041d7b6d
nehu3n <developer.nh3@gmail.com>
2025-08-01 02:04:17 -0300

    feat: Add upload commands and cache path

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,9 @@ import type { ParseClient } from "seyfert";
 import { Client } from "seyfert";
 
 const client = new Client();
-client.start();
+client
+	.start()
+	.then(() => client.uploadCommands({ cachePath: "./commands.json" }));
 
 declare module "seyfert" {
 	interface UsingClient extends ParseClient<Client<true>> {}
