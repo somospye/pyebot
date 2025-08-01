7721394a5cfd460ac358802ff8853031fb822124
nehu3n <developer.nh3@gmail.com>
2025-08-01 19:48:26 -0300

    feat: Add ban command

---
+++ src/commands/ban.ts
@@ -0,0 +1,84 @@
+import type { CommandContext } from "seyfert";
+import {
+	Command,
+	createStringOption,
+	createUserOption,
+	Declare,
+	Embed,
+	Options,
+} from "seyfert";
+import { EmbedColors } from "seyfert/lib/common";
+
+const options = {
+	user: createUserOption({
+		description: "Usuario a banear",
+		required: true,
+	}),
+	reason: createStringOption({
+		description: "Razón del baneo",
+		required: false,
+	}),
+};
+
+@Declare({
+	name: "ban",
+	description: "Banear a un usuario del servidor",
+	defaultMemberPermissions: ["BanMembers"],
+	botPermissions: ["BanMembers"],
+})
+@Options(options)
+export default class BanCommand extends Command {
+	async run(ctx: CommandContext<typeof options>) {
+		const guild = await ctx.guild();
+
+		if (ctx.author.id === ctx.options.user.id)
+			return ctx.write({
+				content: "✗ No podés banearte a vos mismo.",
+			});
+
+		const memberHighestRole = ctx.member
+			? await ctx.member.roles.highest()
+			: undefined;
+		const targetMember = guild
+			? await guild.members.fetch(ctx.options.user.id)
+			: undefined;
+		const targetHighestRole = targetMember
+			? await targetMember.roles.highest()
+			: undefined;
+
+		if (
+			memberHighestRole &&
+			targetHighestRole &&
+			memberHighestRole.position <= targetHighestRole.position
+		)
+			return ctx.write({
+				content:
+					"✗ No podés banear a un usuario con un rol igual o superior al tuyo.",
+			});
+
+		const reasonOption = ctx.options.reason || "Razón no especificada";
+		const reason = `${reasonOption} | Baneado por ${ctx.author.username}`;
+
+		guild?.bans.create(ctx.options.user.id, {}, reason);
+
+		// TODO: logging
+
+		const successEmbed = new Embed({
+			title: "Usuario baneado",
+			description: `
+            ✓ El usuario **${ctx.options.user.username}** fue baneado correctamente.
+            
+            **Razón:** ${reasonOption}
+            `,
+			color: EmbedColors.Green,
+			footer: {
+				text: `Baneado por ${ctx.author.username}`,
+				icon_url: ctx.author.avatarURL() || undefined,
+			},
+		});
+
+		ctx.write({
+			embeds: [successEmbed],
+		});
+	}
+}
