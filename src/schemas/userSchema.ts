5c9f6ede4b327ddec3f05fc74c47798d6463dde0
nehu3n <developer.nh3@gmail.com>
2025-08-04 00:52:21 -0300

    feat: Add warns field to users schema

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,7 +1,22 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, jsonb, pgTable, varchar } from "drizzle-orm/pg-core";
+
+type User = {
+	id: string;
+	bank: number;
+	cash: number;
+	warns: Warn[];
+};
+
+export type Warn = {
+	reason: string;
+	warn_id: number; // incremental
+	moderador: User;
+	timestamp: string; // Date ISO
+};
 
 export const users = pgTable("users", {
 	id: varchar("id", { length: 50 }).primaryKey().notNull().unique(),
 	bank: integer("bank").notNull().default(0),
 	cash: integer("cash").notNull().default(0),
+	warns: jsonb("warns").$type<Warn[]>().default([]),
 });
