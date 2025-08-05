b5b5caabd26f2983b24c57bbdefa9a944db1c897
Elisiei Yehorov <yehorovye@disroot.org>
2025-08-05 21:49:47 +0200

    feat: nix flake

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,14 @@
 import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
+if (!process.env.DATABASE_URL)
+  throw Error("missing 'DATABASE_URL' env variable");
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/schemas/index.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL,
   },
 });
